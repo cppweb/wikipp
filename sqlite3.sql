ab97b353bc7c76388e4df0ae7db396be66834c7f
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2008-10-28 19:47:07 +0000

    Replaced tabs (\t) with blanks ' ' in order to workaround libdbi-drivers 8.2 bug in sqlite3 backend.
    git-svn-id: https://svn.code.sf.net/p/cppcms/code/wikipp/trunk@517 831565cc-8e21-4ad5-8334-98019c97bd36

--- sqlite3.sql
+++ sqlite3.sql
@@ -5,39 +5,39 @@ drop table if exists users;
 drop table if exists options;
 
 create table options (
-	lang varchar(16) not null,
-	name varchar(32) not null,
-	value text not null,
-	unique(lang,name)
+    lang varchar(16) not null,
+    name varchar(32) not null,
+    value text not null,
+    unique(lang,name)
 );
 
 create table users (
-	id integer primary key autoincrement not null,
-	username varchar(32) unique not null,
-	password varchar(32) not null
+    id integer primary key autoincrement not null,
+    username varchar(32) unique not null,
+    password varchar(32) not null
 ) ;
 
 create table pages (
-	id integer primary key autoincrement not null,
-	lang varchar(16) not null,
-	slug varchar(128) not null,
-	title varchar(256) not null,
-	content text not null,
-	sidebar text not null,
-	users_only integer not null,
-	unique (lang,slug)
+    id integer primary key autoincrement not null,
+    lang varchar(16) not null,
+    slug varchar(128) not null,
+    title varchar(256) not null,
+    content text not null,
+    sidebar text not null,
+    users_only integer not null,
+    unique (lang,slug)
 );
 
 
 create table history (
-	id integer not null,
-	version integer not null,
-	created datetime not null,
-	title varchar(256) not null,
-	content text not null,
-	sidebar text not null,
-	unique(id,version),
-	primary key(id,version)
+    id integer not null,
+    version integer not null,
+    created datetime not null,
+    title varchar(256) not null,
+    content text not null,
+    sidebar text not null,
+    unique(id,version),
+    primary key(id,version)
 );
 
 commit;
